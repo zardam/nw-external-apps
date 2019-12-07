e9003ae9575e4290d3cb519a9d6b94a8c04d39fe
Damien Nicolet <zardam@gmail.com>
2019-12-07 13:31:02 +0100

    Some documentation

--- apps/apps.js
+++ apps/apps.js
@@ -2,8 +2,8 @@
 
 angular.module('nwas').service('apps', function() {
   return [
-    { name: "KhiCAS", description: {en: "Computer algebra system", fr: "Système de calcul formel"}, link: "https://www-fourier.ujf-grenoble.fr/~parisse/" },
-    { name: "Periodic", description: {en: "Periodic table of elements", fr: "Tableau périodique des éléments"}, link: "https://bitbucket.org/m4x1m3/nw-atom" },
-    { name: "Nofrendo", description: {en: "NES emulator", fr: "Émulateur NES"}, link: "https://github.com/zardam/numworks-nofrendo" },
+    { name: "KhiCAS", description: {en: "Computer algebra system", fr: "Système de calcul formel"} },
+    { name: "Periodic", description: {en: "Periodic table of elements", fr: "Tableau périodique des éléments"} },
+    { name: "Nofrendo", description: {en: "NES emulator", fr: "Émulateur NES"} },
   ];
 });
